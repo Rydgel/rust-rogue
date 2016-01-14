44323407584a9e7437878daf5510bb7fdecad6bd
Jérôme Mahuet <jerome.mahuet@gmail.com>
2016-01-14 13:46:10 +0100

    learning piston

---
+++ src/game.rs
@@ -0,0 +1,75 @@
+use piston::input::*;
+use graphics::*;
+use opengl_graphics::GlGraphics;
+
+pub struct Game {
+    rotation: f64,
+    x: f64,
+    y: f64,
+    left: bool,
+    right: bool,
+    up: bool,
+    down: bool,
+}
+
+impl Game {
+    pub fn new() -> Game {
+        Game {
+            rotation: 0.0,
+            x: 300.0,
+            y: 300.0,
+            left: false,
+            right: false,
+            up: false,
+            down: false
+        }
+    }
+
+    pub fn update(&mut self, upd: UpdateArgs) {
+        self.rotation += 15.0 * upd.dt;
+        self.move_player(upd.dt);
+    }
+
+    pub fn key_press(&mut self, k: Key) {
+        match k {
+            Key::Up => self.up = true,
+            Key::Down => self.down = true,
+            Key::Right => self.right = true,
+            Key::Left => self.left = true,
+            _ => {}
+        }
+    }
+
+    pub fn key_release(&mut self, k: Key) {
+        match k {
+            Key::Up => self.up = false,
+            Key::Down => self.down = false,
+            Key::Right => self.right = false,
+            Key::Left => self.left = false,
+            _ => {}
+        }
+    }
+
+    fn move_player(&mut self, dt: f64) {
+        if self.up {
+            self.y -= 450.0 * dt;
+        }
+        if self.down {
+            self.y += 450.0 * dt
+        }
+        if self.right {
+            self.x += 450.0 * dt;
+        }
+        if self.left {
+            self.x -= 450.0 * dt;
+        }
+    }
+
+    pub fn render(&mut self, c: Context, g: &mut GlGraphics) {
+        clear([0.0, 0.0, 0.0, 1.0], g);
+        let center = c.transform.trans(self.x, self.y);
+        let square = rectangle::square(0.0, 0.0, 100.0);
+        let red = [1.0, 0.0, 0.0, 1.0];
+        rectangle(red, square, center.rot_rad(self.rotation).trans(-50.0, -50.0), g);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,15 @@ extern crate opengl_graphics;
 extern crate piston;
 extern crate rand;
 
+mod game;
+
 use glutin_window::GlutinWindow;
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{Events, EventLoop};
 use piston::input::{Button, Event, Input, RenderEvent};
 use piston::window::WindowSettings;
+
+use game::Game;
 
 const OPEN_GL: OpenGL = OpenGL::V3_2;
 
@@ -32,23 +36,25 @@ fn main() {
     // Event handling
     let mut events = window.events().ups(60).max_fps(60);
 
+    // Game state
+    let mut game = Game::new();
+
     while let Some(e) = events.next(&mut window) {
         match e {
             Event::Input(Input::Press(Button::Keyboard(key))) => {
-                // println!("{:?}", key);
-                // game.key_press(key);
+                game.key_press(key);
             }
 
             Event::Input(Input::Release(Button::Keyboard(key))) => {
-                // game.key_release(key);
+                game.key_release(key);
             }
 
             Event::Render(args) => {
-                // gl.draw(args.viewport(), |c, g| game.render(c, g));
+                gl.draw(args.viewport(), |c, g| game.render(c, g));
             }
 
             Event::Update(args) => {
-                // game.update(args.dt);
+                game.update(args);
             }
 
             _ => {}
